50ebd19f13669d3aa91bce86a54975313481b4d5
Ilguiz Latypov <ilguiz_latypov@manulife.com>
2019-01-06 00:24:37 -0500

    Parameterize.  Use cygstart to start a Command Prompt terminal in order to let ffmpeg/ffplay be controlled by keystrokes.

--- play.bat
+++ play.bat
@@ -1,3 +1,5 @@
+@rem Usage: [cygstart] play.bat ru-digits.wav
+
 @rem https://trac.ffmpeg.org/ticket/6891
 @rem set SDL_AUDIODRIVER=directsound or winmm
 set SDL_AUDIODRIVER=winmm
--- record.bat
+++ record.bat
@@ -1,3 +1,4 @@
+@rem Usage: [cygstart] record.bat ru-digits
 
 ffmpeg -list_devices true -f dshow -i dummy
 @rem [...]
@@ -6,5 +7,5 @@ ffmpeg -list_devices true -f dshow -i dummy
 @rem [dshow @ 00000000005d1140]     Alternative name "@device_cm_{33D9A762-90C8-11D0-BD43-00A0C911CE86}\Microphone Array (Realtek High "
 @rem [...]
 
-ffmpeg -f dshow -i audio="Microphone Array (Realtek High " NAME.wav
+ffmpeg -f dshow -i audio="Microphone Array (Realtek High " %1.wav
 
